f19dc1a4d9a7d86b48fa9a92c9f5f19159717d54
skr <kumarsugam2609@gmail.com>
2023-01-19 16:14:04 +0530

    mock11

--- index.js
+++ index.js
@@ -11,17 +11,20 @@ app.use(express.json())
 app.use(cors({origin : "*"}))
 
 app.get("/", (req, res) => {
-    res.send("Welcome ")
+    res.send("Welcome to this api for sign up and login(signin) ")
 })
 
 
 app.post("/signup", async (req, res) => {
-    console.log(req.body)
+    console.log("req.body",req.body)
     const {email, password} = req.body;
+
+    console.log("req email",email,"req password",password)
+
     const userPresent = await UserModel.findOne({email})
     
     if(userPresent?.email){
-        res.send("Try loggin in, already exist")
+        res.send("User already exist, Please go to Sign in page")
     }
     else{
         try{    const user = new UserModel({email,password})
@@ -30,7 +33,7 @@ app.post("/signup", async (req, res) => {
                     }
        catch(err){
             console.log(err)
-            res.send("Something went wrong, pls try again later")
+            res.send("Something went wrong, please try again later")
        }
     }
     
@@ -66,10 +69,10 @@ app.get("/signin", async (req, res) => {
 app.listen(7000, async () => {
     try{
         await connection;
-        console.log("Connected to DB Successfully")
+        console.log("Connected to MongoDB Successfully")
     }
     catch(err){
-        console.log("Error connecting to DB")
+        console.log("Error with mongoDB")
         console.log(err)
     }
     console.log("Listening on PORT 7000")
